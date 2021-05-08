d05841d6125451f4e7ff3e804bf563ef7bf16e2d
alan-966 <alan.gorbunov@mail.ru>
2021-05-09 00:29:00 +0300

    gulp config

--- gulpfile.js
+++ gulpfile.js
@@ -30,7 +30,18 @@ let { src, dest } = require('gulp'),
 	browsersync = require('browser-sync').create(),
 	fileinclude = require('gulp-file-include'),
 	del = require('del'),
-	scss = require('gulp-sass');
+	scss = require('gulp-sass'),
+	autoprefixer = require('gulp-autoprefixer'),
+	group_media = require('gulp-group-css-media-queries'),
+	clean_css = require('gulp-clean-css'),
+	rename = require('gulp-rename'),
+	uglify = require('gulp-uglify-es').default,
+	imagemin = require('gulp-imagemin'),
+	webp = require('gulp-webp'),
+	webphtml = require('gulp-webp-html'),
+	webpcss = require('gulp-webpcss'),
+	ttf2woff = require('gulp-ttf2woff'),
+	ttf2woff2 = require('gulp-ttf2woff2');
 
 function browserSync(params)
 {
@@ -43,10 +54,21 @@ function browserSync(params)
 	})
 }
 
+function fonts()
+{
+	src(path.src.fonts)
+		.pipe(ttf2woff())
+		.pipe(dest(path.build.fonts));
+	return src(path.src.fonts)
+		.pipe(ttf2woff2())
+		.pipe(dest(path.build.fonts));
+}
+
 function html()
 {
 	return src(path.src.html)
 		.pipe(fileinclude())
+		.pipe(webphtml())
 		.pipe(dest(path.build.html))
 		.pipe(browsersync.stream())
 }
@@ -59,13 +81,68 @@ function css()
 				outputStyle: 'expanded'
 			})
 		)
+		.pipe(group_media())
+		.pipe(
+			autoprefixer({
+				overrideBrowserslist: ['last 5 versions'],
+				cascade: true,
+			})
+		)
+		.pipe(webpcss())
 		.pipe(dest(path.build.css))
+		.pipe(clean_css())
+		.pipe(
+			rename({
+				extname: '.min.css',
+			})
+		)
+		.pipe(dest(path.build.css))
+		.pipe(browsersync.stream())
+}
+
+function js()
+{
+	return src(path.src.js)
+		.pipe(fileinclude())
+		.pipe(dest(path.build.js))
+		.pipe(uglify())
+		.pipe(
+			rename({
+				extname: '.min.js',
+			})
+		)
+		.pipe(dest(path.build.js))
+		.pipe(browsersync.stream())
+}
+
+function images()
+{
+	return src(path.src.img)
+		.pipe(
+			webp({
+				quality: 70,
+			})
+		)
+		.pipe(dest(path.build.img))
+		.pipe(src(path.src.img))
+		.pipe(
+			imagemin({
+				progressive: true,
+				svgoPlagins: [{ removeViewBox: false }],
+				interlaced: true,
+				optimizationLevel: 3, // 0 to 7
+			})
+		)
+		.pipe(dest(path.build.img))
 		.pipe(browsersync.stream())
 }
 
 function watchFiles()
 {
 	gulp.watch([path.watch.html], html);
+	gulp.watch([path.watch.css], css);
+	gulp.watch([path.watch.js], js);
+	gulp.watch([path.watch.img], images);
 }
 
 function clean()
@@ -73,10 +150,13 @@ function clean()
 	return del(path.clean);
 }
 
-let build = gulp.series(clean, gulp.parallel(css, html));
+let build = gulp.series(clean, gulp.parallel(js, css, html, images, fonts));
 let watch = gulp.parallel(build, watchFiles, browserSync);
 
 
+exports.fonts = fonts;
+exports.images = images;
+exports.js = js;
 exports.css = css;
 exports.html = html;
 exports.build = build;
