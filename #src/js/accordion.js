3fa727750594cacb317f26c48f70271e5989ab10
alan-966 <alan.gorbunov@mail.ru>
2021-09-13 15:39:14 +0300

    test accordion

--- #src/js/accordion.js
+++ #src/js/accordion.js
@@ -5,9 +5,12 @@ for (let i = 0; i < acc.length; i++)
 		this.classList.toggle("active");
 		// const acContent = this.nextElementSibling;
 		const acContent = this.querySelector('.accordion_content');
-		if (acContent.style.maxHeight){
+		if (acContent.style.maxHeight)
+		{
 			acContent.style.maxHeight = null;
-		} else {
+		}
+		else
+		{
 			acContent.style.maxHeight = acContent.scrollHeight + "px";
 		}
 	});
