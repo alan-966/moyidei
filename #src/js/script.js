a9b4bb68e9b1c9513654b5543d94b18d04c12bb1
alan-966 <alan.gorbunov@mail.ru>
2022-02-28 14:43:41 +0300

    мелочь

--- #src/js/script.js
+++ #src/js/script.js
@@ -2,8 +2,7 @@ window.addEventListener('load', () => {
 	@@include('pointer.js')
 	@@include('accordion.js')
 	@@include('alert.js')
-	@@include('popup.js')
 });
-
+@@include('popup.js')
 @@include('slider.js')
 @@include('webp.js')
