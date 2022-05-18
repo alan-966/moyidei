76fd22bf2e9c5736ea105cf66fd93339df3e48a7
alan-966 <alan.gorbunov@mail.ru>
2022-05-19 01:14:56 +0300

    Начал адаптив шапки

--- #src/js/menu.js
+++ #src/js/menu.js
@@ -21,6 +21,18 @@ for (let i = 0; i < linkWithSub.length; i++)
 }
 
 window.onclick = function(e) {
-	const activeItem = document.getElementsByClassName('active_mobile_item')[0];
-	activeItem.classList.remove('active_mobile_item');
+	const activeItems = document.getElementsByClassName('active_mobile_item');
+	for (let i = 0; i < activeItems.length; i++)
+	{
+		activeItems[i].classList.remove('active_mobile_item');
+	}
+}
+
+const burgerBtn = document.getElementsByClassName('header_burger');
+if (burgerBtn.length)
+{
+	burgerBtn[0].addEventListener('click', function(e) {
+		const menu = document.getElementsByClassName('header_menu_wrapper');
+		if (menu.length) menu[0].classList.toggle('active');
+	})
 }
