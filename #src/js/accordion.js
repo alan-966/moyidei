76b55f1485ed61a8d96b3dc4ff0126b37c4aa5ba
alan-966 <alan.gorbunov@mail.ru>
2022-05-21 01:58:11 +0300

    burger menu

--- #src/js/accordion.js
+++ #src/js/accordion.js
@@ -3,15 +3,12 @@ for (let i = 0; i < acc.length; i++)
 {
 	acc[i].addEventListener("click", function() {
 		this.classList.toggle("active");
-		// const acContent = this.nextElementSibling;
+
 		const acContent = this.querySelector('.accordion_content');
+		
 		if (acContent.style.maxHeight)
-		{
 			acContent.style.maxHeight = null;
-		}
 		else
-		{
 			acContent.style.maxHeight = acContent.scrollHeight + "px";
-		}
 	});
 }
--- #src/js/menu.js
+++ #src/js/menu.js
@@ -1,7 +1,8 @@
 const mobileMenu = document.querySelector('.mobile_menu_wrapper');
+const mobileSubmenus = document.querySelectorAll('.mobile_submenu');
 const body = document.querySelector('body');
 
-const linksWithSub = document.getElementsByClassName('has_sub');
+const linksWithSub = document.querySelectorAll('.has_sub');
 for (let i = 0; i < linksWithSub.length; i++)
 {
 	linksWithSub[i].addEventListener('click', function(e) {
@@ -10,7 +11,7 @@ for (let i = 0; i < linksWithSub.length; i++)
 
 		const currentItem = linksWithSub[i].parentNode;
 
-		const activeItems = document.getElementsByClassName('active_mobile_item');
+		const activeItems = document.querySelectorAll('.active_mobile_item');
 		for (let j = 0; j < activeItems.length; j++)
 		{
 			const activeItem = activeItems[j];
@@ -22,23 +23,57 @@ for (let i = 0; i < linksWithSub.length; i++)
 	});
 }
 
+const mLinksWithSub = document.querySelectorAll('.has_mobile_sub');
+for (let i = 0; i < mLinksWithSub.length; i++)
+{
+	mLinksWithSub[i].addEventListener('click', function(e) {
+		e.preventDefault();
+		e.stopPropagation();
+
+		const currentItem = mLinksWithSub[i].parentNode;
+		currentItem.classList.toggle('active_mobile_item');
+
+		const submenu = currentItem.lastElementChild;
+		if (submenu.style.maxHeight)
+			submenu.style.maxHeight = null;
+		else
+			submenu.style.maxHeight = submenu.scrollHeight + "px";
+	});
+}
+
 window.onclick = function(e) {
-	const activeItems = document.getElementsByClassName('active_mobile_item');
+	const activeItems = document.querySelectorAll('.active_mobile_item');
 	for (let i = 0; i < activeItems.length; i++)
 	{
 		activeItems[i].classList.remove('active_mobile_item');
 	}
 	mobileMenu.classList.remove('active');
 	body.classList.remove('lock');
+
+	for (let i = 0; i < mobileSubmenus.length; i++)
+	{
+		mobileSubmenus[i].style.maxHeight = null;
+	}
 }
 
-const burgerBtns = document.getElementsByClassName('header_burger');
+const burgerBtns = document.querySelectorAll('.header_burger');
 for (let i = 0; i < burgerBtns.length; i++)
 {
 	burgerBtns[i].addEventListener('click', function(e) {
 		e.stopPropagation();
 		mobileMenu.classList.toggle('active');
 		body.classList.toggle('lock');
+		
+		for (let i = 0; i < mobileSubmenus.length; i++)
+		{
+			mobileSubmenus[i].style.maxHeight = null;
+		}
+
+		const activeItems = document.querySelectorAll('.active_mobile_item');
+		for (let i = 0; i < activeItems.length; i++)
+		{
+			activeItems[i].classList.remove('active_mobile_item');
+		}
 	})
 }
 
