da7b49fd5196a8c92c6e951776f62bf0333f6893
alan-966 <alan.gorbunov@mail.ru>
2022-09-25 02:31:24 +0300

    Исправил ошибки в консоле. Изменил логику блока с указателем

--- #src/js/pointer.js
+++ #src/js/pointer.js
@@ -1,30 +1,22 @@
-const images = [
-	'/img/pointer/1.png',
-	'/img/pointer/2.png',
-	'/img/pointer/3.png',
-	'/img/pointer/4.png',
-	'/img/pointer/5.png',
-	'/img/pointer/6.png',
-	'/img/pointer/7.png',
-	'/img/pointer/8.png',
-	'/img/pointer/9.png',
-	'/img/pointer/10.png',
-];
-
-const teamBlockImg = document.querySelector('.team_block_img_wrapper');
-const source = teamBlockImg.querySelector('source');
-const image = teamBlockImg.querySelector('img');
-
 const teamBlockContent = document.querySelector('.team_block_content');
-const height = teamBlockContent.scrollHeight;
-const oneChank = height / 10;
 
-teamBlockContent.addEventListener("mousemove", (e) => {
-	const mousePos = e.pageY - teamBlockContent.offsetTop + 1;
-	const imgIndex = Math.floor(mousePos / oneChank);
-	if (imgIndex >= 0 && imgIndex < images.length)
-	{
-		source.setAttribute('srcset', images[imgIndex]);
-		image.setAttribute('src', images[imgIndex]);
-	}
-});
+if (teamBlockContent) {
+	const height = teamBlockContent.scrollHeight;
+	const oneChank = height / 10;
+	let prevImgIndex = 0;
+
+	const images = document.querySelectorAll('.team_block_img');
+
+	teamBlockContent.addEventListener("mousemove", e => {
+		const mousePos = e.pageY - teamBlockContent.offsetTop + 1;
+		const imgIndex = Math.floor(mousePos / oneChank);
+
+		if (imgIndex >= 0 && imgIndex < images.length) {
+			if (imgIndex !== prevImgIndex) {
+				images[prevImgIndex].classList.remove('active_img');
+				images[imgIndex].classList.add('active_img');
+			}
+			prevImgIndex = imgIndex;
+		}
+	});
+};
